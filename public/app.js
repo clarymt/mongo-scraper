feadbaa9c87c8239e081ecd7dbf224513d241938
clarymt <52682135+clarymt@users.noreply.github.com>
2019-12-16 16:10:50 -0500

    added some code that may append notes to seperate page will complete buildout for this function but now transitioning to stlyizing

--- public/app.js
+++ public/app.js
@@ -76,3 +76,34 @@ $(document).on("click", "#savenote", function () {
   // Also, remove the values entered in the input and textarea for note entry
   $("#bodyinput").val("");
 });
+
+
+
+
+/*function handleArticleSave() {
+  // This function is triggered when the user wants to save an article
+  // When we rendered the article initially, we attached a javascript object containing the headline id
+  // to the element using the .data method. Here we retrieve that.
+  var articleToSave = $(this)
+    .parents(".card")
+    .data();
+
+  // Remove card from page
+  $(this)
+    .parents(".card")
+    .remove();
+
+  articleToSave.saved = true;
+  // Using a patch method to be semantic since this is an update to an existing record in our collection
+  $.ajax({
+    method: "PUT",
+    url: "/api/headlines/" + articleToSave._id,
+    data: articleToSave
+  }).then(function(data) {
+    // If the data was saved successfully
+    if (data.saved) {
+      // Run the initPage function again. This will reload the entire list of articles
+      initPage();
+    }
+  });
+}*/
