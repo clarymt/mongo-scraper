5660fbc0d53c3e881ed59c73c925e7bea71d9ce3
clarymt <52682135+clarymt@users.noreply.github.com>
2020-01-02 20:48:02 -0500

    troubleshooting heroku

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ var cheerio = require("cheerio");
 // Require all models
 var db = require("./models");
 
-var PORT = 3002;
+var PORT = process.env.PORT || 3002;
 
 // Initialize Express
 var app = express();
