201b766a480c0d6304ae27a39b84ac5cd9e8e8bc
clarymt <52682135+clarymt@users.noreply.github.com>
2020-01-04 13:31:21 -0500

    heroku not working

--- server.js
+++ server.js
@@ -34,55 +34,55 @@ mongoose.connect("mongodb://localhost/mongoTechScraperDB", { useNewUrlParser: tr
 // A GET route for scraping the echoJS website
 app.get("/scrape", function (req, res) {
   // First, we grab the body of the html with axios
-  // axios.get("https://time.com/section/tech/").then(function (response) {
-  //   // Then, we load that into cheerio and save it to $ for a shorthand selector
-  //   var $ = cheerio.load(response.data);
+  axios.get("https://time.com/section/tech/").then(function (response) {
+    // Then, we load that into cheerio and save it to $ for a shorthand selector
+    var $ = cheerio.load(response.data);
 
-  //   // Now, we grab every h2 within an article tag, and do the following:
-  //   $("div.article-info").each(function (i, element) {
-  //     // Save an empty result object
-  //     var result = {};
-  //     var url = "https://time.com/"
-  //     // Add the text and href of every link, and save them as properties of the result object
-  //     result.title = $(this)
-  //       .children("h3")
-  //       .children("a")
-  //       .text();
-  //     result.link = url + $(this)
-  //       .children("h3")
-  //       .children("a")
-  //       .attr("href");
-  //     result.summary = $(this)
-  //       .find("div.desktop-only")
-  //       .text();
-  //    console.log(result);
+    // Now, we grab every h2 within an article tag, and do the following:
+    $("div.article-info").each(function (i, element) {
+      // Save an empty result object
+      var result = {};
+      var url = "https://time.com/"
+      // Add the text and href of every link, and save them as properties of the result object
+      result.title = $(this)
+        .children("h3")
+        .children("a")
+        .text();
+      result.link = url + $(this)
+        .children("h3")
+        .children("a")
+        .attr("href");
+      result.summary = $(this)
+        .find("div.desktop-only")
+        .text();
+     console.log(result);
 
-  //     // Create a new Article using the `result` object built from scraping
-  //     db.Article.findOne({ title: result.title })
-  //       .then(function (dbArticle) {
-  //         if (!dbArticle) {
-  //           db.Article.create(result)
-  //             .then(function (dbArticle) {
-  //               // View the added result in the console
-  //               console.log(dbArticle);
-  //             })
-  //             .catch(function (err) {
-  //               // If an error occurred, log it
-  //               console.log(err);
-  //             });
-  //         }
-  //       })
-  //       .catch(function (dbArticle) {
-  //         console.log("---" + dbArticle)
-  //       });
+      // Create a new Article using the `result` object built from scraping
+      db.Article.findOne({ title: result.title })
+        .then(function (dbArticle) {
+          if (!dbArticle) {
+            db.Article.create(result)
+              .then(function (dbArticle) {
+                // View the added result in the console
+                console.log(dbArticle);
+              })
+              .catch(function (err) {
+                // If an error occurred, log it
+                console.log(err);
+              });
+          }
+        })
+        .catch(function (dbArticle) {
+          console.log("---" + dbArticle)
+        });
 
 
 
-  //   });
+    });
 
-  //   // Send a message to the client
-  //   res.redirect("/");
-  // });
+    // Send a message to the client
+    res.redirect("/");
+  });
 });
 
 // Route for getting all Articles from the db
