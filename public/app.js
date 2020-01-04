d59eebee1ee907bcea4419dab191e976d14ee336
clarymt <52682135+clarymt@users.noreply.github.com>
2020-01-04 12:31:42 -0500

    heroku sucks

--- public/app.js
+++ public/app.js
@@ -1,7 +1,8 @@
 // Grab the articles as a json
 $.getJSON("/articles", function (data) {
   // For each one
-  for (var i = 0; i < data.length; i++) {
+//  for (var i = 0; i < data.length; i++) {
+  for (var i = 0; i < 10; i++) {
     // Display the apropos information on the page
     //  $("#articles").append("<p data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + data[i].link + "</p>");
     $("#articles").append(`
